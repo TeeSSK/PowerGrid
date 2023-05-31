1ebd2cc67e6c78a92f27ca5d136a199e218fcd31
Sukij Sunitsakul <sukij44800@gmail.com>
2023-05-31 20:18:53 +0700

    count execute time

--- powerGrid.py
+++ powerGrid.py
@@ -4,6 +4,7 @@ import time
 
 
 def main():
+    start_time_np = time.time()
     if len(sys.argv) != 3:
         print("Usage: python3 powerGrid.py <inputPath> <outputPath>")
         return
@@ -37,7 +38,7 @@ def main():
 
     for i in range(n):
         # sum of continuos nodes in range min 1 max 6
-        constraint = solver.Constraint(1, 6, 'eq'+str(i))
+        constraint = solver.Constraint(1, n, 'eq'+str(i))
         for adnode in edges[i]:
             constraint.SetCoefficient(vertics[adnode], 1)
         constraint.SetCoefficient(vertics[i], 1)
@@ -47,26 +48,32 @@ def main():
         objective.SetCoefficient(vertics[i], 1)
     objective.SetMinimization()
 
-    start_time_np = time.time()
+    # start_time_np = time.time()
     solver.Solve()
-    end_time_np = time.time()
-    print("LP time:", end_time_np - start_time_np)
+    # end_time_np = time.time()
+    # print("LP time:", end_time_np - start_time_np)
 
     minimum_node = str(int(objective.Value()))
-    print('Minimum value:', minimum_node)
-    print('Vertics:', [int(vertics[i].solution_value()) for i in range(n)])
+    # print('Minimum value:', minimum_node)
+    # print('Vertics:', [int(vertics[i].solution_value()) for i in range(n)])
     answer = ''
     for i in range(n):
         answer += str(int(vertics[i].solution_value()))
-    print('Answer:', answer)
+    # print('Answer:', answer)
 
     file_output = open(path_out, "w")
     file_output.write(minimum_node+':'+answer)
     file_output.close()
 
+    end_time_np = time.time()
+    print("LP time:", end_time_np - start_time_np)
+
 
 if __name__ == '__main__':
     main()
 
-# python powerGrid.py ./data/input/rand-50-200.txt ./data/output/outtest.txt
-# docker run -v "$(pwd)"/data/input:/input -v "$(pwd)"/data/output:/output powergrid /input/rand-50-200.txt /output/outtest.txt
+# python powerGrid.py data/input/tree-30-29 data/output/outtest.txt
+# time docker run -v "$(pwd)"/data/input:/input -v "$(pwd)"/data/output:/output teessk/powergrid /input/tree-30-29 /output/tree-30-29.txt
+# time docker run -v "$(pwd)"/data/input:/input -v "$(pwd)"/data/output:/output teessk/powergrid /input/spec-6.dat /output/spec-6.txt
+# time docker run -v "$(pwd)"/data/input:/input -v "$(pwd)"/data/output:/output teessk/powergrid /input/grid-12-17 /output/grid-12-17.txt
+# time docker run -v "$(pwd)"/data/input:/input -v "$(pwd)"/data/output:/output teessk/powergrid /input/rand-5-7 /output/rand-5-7.txt
